e5c671f29859d06d3977e3a262afa042b43df88f
Guillaume <levasseurguillaume91@gmail.com>
2023-01-09 12:11:48 +0100

    UPDATE ex13 & 14

--- 13_clickHandler.js
+++ 13_clickHandler.js
@@ -2,6 +2,6 @@ document.getElementById("clickme").addEventListener("click", (e) = () => {
         let myP = document.createElement("p");
         myP.setAttribute("id", "result")
         myP.innerHTML = "clicked";
-        document.querySelector("div#content").appendChild(myP);
+        document.querySelector("div.content").appendChild(myP);
     }
 );
--- 14_hoverHandler.js
+++ 14_hoverHandler.js
@@ -6,5 +6,5 @@ document.getElementById("hoverme").addEventListener("mouseover", (e) = () => {
     let myP = document.createElement("p");
     myP.setAttribute("id", "result");
     myP.innerHTML = "hovered";
-    document.querySelector("div#content").appendChild(myP);
+    document.querySelector("div.content").appendChild(myP);
 })
