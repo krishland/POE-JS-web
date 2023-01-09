324c91f8afb872533f64724beeab7afba7d417f3
Guillaume <levasseurguillaume91@gmail.com>
2023-01-09 15:18:35 +0100

    DONE ex16

---
+++ 16_contactForm.js
@@ -0,0 +1,17 @@
+// récupérer les champs d’un formulaire possédant les champs suivants:
+// <input type=”text” name=”name” />
+// <input type=”number” name=”age” />
+// <input type=”date” name=”birthdate” />
+// <button>valider</button>
+// afficher le nom et la valeur dans une balise p possédant l’id “result” sous la forme suivante:
+// name: Guilian, age: 42, birthdate: 2000-12-09
+
+document.querySelector("button").addEventListener("click", (e) = () => {
+    let myName = [...document.getElementsByName("name")];
+    let myAge = [...document.getElementsByName("age")];
+    let myBirth = [...document.getElementsByName("birthdate")];
+    let myP = document.createElement("p");
+    myP.setAttribute("id", "result");
+    myP.innerText = `name: ${myName[0].value}, age: ${myAge[0].value}, birthdate: ${myBirth[0].value}`;
+    document.body.appendChild(myP);
+})
