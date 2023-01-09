88d99a6427f519f4d58d00d050d0310eb269cd34
Guillaume <levasseurguillaume91@gmail.com>
2023-01-09 18:36:53 +0100

    UPDATE ex16

--- 19_keybinding.js
+++ 19_keybinding.js
@@ -1,21 +1,18 @@
-// créez un carré de 50px sur 50px en html (aidez vous de backgroundColor pour la visualisation), ce carré possèdera 
-// une position: absolute, ce carré possédera l’id “square”
-// déplacer le carré de 10px dans la direction donné par les touches du clavier (haut bas gauche droite
-document.body.style.margin = "0px"
-let myDiv = document.createElement("div")
-myDiv.style.width = "100vw"
-myDiv.style.height = "100vh"
-document.body.appendChild(myDiv)
+let mySquare = document.getElementById("square");
+mySquare.style.left = "0px";
+mySquare.style.top = "0px";
 
-let mySquare = document.createElement("div")
-mySquare.setAttribute("id", "square")
-mySquare.style.width = "50px"
-mySquare.style.height = "50px"
-mySquare.style.left = "0px" 
-mySquare.style.top = "0px" 
-mySquare.style.backgroundColor = "red"
-myDiv.appendChild(mySquare)
 
-document.body.addEventListener("keydown", (e) = () => {
-    mySquare.style.top = mySquare.style.top + "50px"
+document.body.addEventListener('keydown', (e) => {
+    let x = parseInt(mySquare.style.left, 10);
+    let y = parseInt(mySquare.style.top, 10);
+    console.log(x)
+    console.log(y)
+    console.log(e.key)
+    if (e.key === "ArrowUp") {y -= 10;}
+	if (e.key === "ArrowDown") {y += 10;}
+    if (e.key === "ArrowLeft") {x -= 10;}
+    if (e.key === "ArrowRight") {x += 10;}		
+    mySquare.style.left = x + "px";
+    mySquare.style.top = y + "px";
 })
