4f3efa11964b8ba4006bbb35f9ece4597e86261c
Guillaume <levasseurguillaume91@gmail.com>
2023-01-09 12:08:55 +0100

    DONE ex14

--- 13_clickHandler.js
+++ 13_clickHandler.js
@@ -1,5 +1,6 @@
 document.getElementById("clickme").addEventListener("click", (e) = () => {
         let myP = document.createElement("p");
+        myP.setAttribute("id", "result")
         myP.innerHTML = "clicked";
         document.querySelector("div#content").appendChild(myP);
     }
