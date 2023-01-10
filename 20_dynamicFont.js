33bc4aefc5ed3bd3f80ed9e7e2ca959ebe02657b
Guillaume <levasseurguillaume91@gmail.com>
2023-01-10 11:26:56 +0100

    UPDATE ex20

--- 20_dynamicFont.js
+++ 20_dynamicFont.js
@@ -1,9 +1,4 @@
-// ajouter un champ de formulaire de type nombre, ce champs devra posséder l’id “fontsize”
-// Appliquer en tant que fontSize, la valeur du champ aux éléments possédant la classe “controlled_fontsize”
-
-let myForm = document.getElementById("input");
-// myForm.setAttribute("type", "number");
-// myForm.setAttribute("id", "fontsize");
+let myForm = document.getElementById("fontsize");
 
 document.getElementById("fontsize").addEventListener("input", () => {
     let elems = document.getElementsByClassName("controlled_fontsize")
