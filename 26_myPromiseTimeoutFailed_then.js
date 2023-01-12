5c5d40be72075ed672f252b92d3b13a2f841e75e
Guillaume <levasseurguillaume91@gmail.com>
2023-01-12 12:13:52 +0100

    CREATE ex 26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -3,3 +3,26 @@
 // // un click sur le bouton#fail ajoutera un élément p.error
 // La méthode de gestion d’erreur sera .catch()
 // astuce: utilisez les classes css pour changer la couleur du texte et faciliter le développemen
+
+const main = require("./25_myPromiseTimeoutFailed.js");
+
+document.querySelectorAll("button").addEventListener("click", () => {
+    console.log(e.target.id)
+    sleepThrow(2000, e.target.id)
+        .then(()=>{
+            let p = document.createElement("p");
+            p.setAttribute("id", "success")
+            p.textContent = "Success";
+            p.style.backgroundColor = "green"
+            document.body.append(p);
+        })
+
+        .catch((e) => {
+            let p = document.createElement("p");
+            p.setAttribute("id", "fail")
+            p.textContent = "Fail";
+            p.style.backgroundColor = "red"
+            document.body.append(p);
+        })
+})
+
