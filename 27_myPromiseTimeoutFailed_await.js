01c9337836744bdc4a56e8a09cdfec743da51d16
Guillaume <levasseurguillaume91@gmail.com>
2023-01-12 16:08:50 +0100

    UPDATE ex27

--- 27_myPromiseTimeoutFailed_await.js
+++ 27_myPromiseTimeoutFailed_await.js
@@ -3,14 +3,14 @@ document.querySelectorAll("button").forEach((i) => {
         try {
             await sleepThrow(2000, e.target.id === "success")
             let p = document.createElement("p");
-            p.setAttribute("id", "success")
+            p.setAttribute("class", "success")
             p.textContent = "Success";
             p.style.color = "green"
             document.body.append(p);
 
         } catch {
             let p = document.createElement("p");
-            p.setAttribute("id", "error")
+            p.setAttribute("class", "error")
             p.textContent = "Fail";
             p.style.color = "red"
             document.body.append(p);
