58320c8713d77a4ee57f419c6cefef43ff920361
Guillaume <levasseurguillaume91@gmail.com>
2023-01-12 12:36:13 +0100

    DONE ex 26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -1,14 +1,6 @@
-// // importez votre exercice 25, utilisez sleep et sleepThrow sur 2 boutons:
-// // un click sur le bouton#success ajoutera un élément p.success
-// // un click sur le bouton#fail ajoutera un élément p.error
-// La méthode de gestion d’erreur sera .catch()
-// astuce: utilisez les classes css pour changer la couleur du texte et faciliter le développemen
-
-const main = require("./25_myPromiseTimeoutFailed.js");
-
-document.querySelectorAll("button").addEventListener("click", () => {
-    console.log(e.target.id)
-    sleepThrow(2000, e.target.id)
+document.querySelectorAll("button").forEach((i) => {
+    i.addEventListener("click", (e) => {
+    sleepThrow(2000, e.target.id === "success")
         .then(()=>{
             let p = document.createElement("p");
             p.setAttribute("id", "success")
@@ -19,10 +11,10 @@ document.querySelectorAll("button").addEventListener("click", () => {
 
         .catch((e) => {
             let p = document.createElement("p");
-            p.setAttribute("id", "fail")
+            p.setAttribute("id", "error")
             p.textContent = "Fail";
             p.style.backgroundColor = "red"
             document.body.append(p);
         })
+    })
 })
-
