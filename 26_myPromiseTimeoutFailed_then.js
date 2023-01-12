2572e76e285daceef8c3b1bcf36712ae7e128dbd
Guillaume <levasseurguillaume91@gmail.com>
2023-01-12 16:04:16 +0100

    UPDATE ex26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -3,15 +3,15 @@ document.querySelectorAll("button").forEach((i) => {
     sleepThrow(2000, e.target.id === "success")
         .then(()=>{
             let p = document.createElement("p");
-            p.setAttribute("id", "success")
+            p.setAttribute("class", "success")
             p.textContent = "Success";
             p.style.color = "green"
             document.body.append(p);
         })
 
-        .catch((e) => {
+        .catch(() => {
             let p = document.createElement("p");
-            p.setAttribute("id", "error")
+            p.setAttribute("class", "error")
             p.textContent = "Fail";
             p.style.color = "red"
             document.body.append(p);
