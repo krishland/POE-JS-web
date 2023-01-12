739b216924e19b11cc9b00f6e03d245b9d2e4a27
Guillaume <levasseurguillaume91@gmail.com>
2023-01-12 11:00:24 +0100

    DONE ex24

--- 24_myPromiseTimeout_await.js
+++ 24_myPromiseTimeout_await.js
@@ -5,3 +5,10 @@
 const sleep = (milliseconds) => { 
     return new Promise(resolve => setTimeout(resolve, milliseconds)) 
 }
+
+document.querySelector("button").addEventListener("click", async () => {
+    await sleep(2000);
+    let p = document.createElement("p");
+    p.textContent = "add-me";
+    document.body.append(p);
+})
